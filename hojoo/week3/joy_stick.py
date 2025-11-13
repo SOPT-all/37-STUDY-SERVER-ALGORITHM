9faf1507a7ad1abd6761e38edb91e2965f6c06a1
hojooo <ghwn5833@gmail.com>
2025-11-14 02:55:29 +0900

    hojoo/week3

---
+++ hojoo/week3/joy_stick.py
@@ -0,0 +1,23 @@
+# 조이스틱 움직여서 원하는 단어 만들기
+# 수직 이동은 최적해와 관계없고 좌우 이동만 관계있음
+# -> 원형에서 변경해야 하는 위치들을 모두 방문하는 최소 좌/우 이동 수
+# 시간 복잡도: O(n)
+
+def solution(name):
+    n = len(name)
+    updown = sum(min(ord(c) - 65, 26 - (ord(c) - 65)) for c in name)
+
+    move = n - 1
+    for i in range(n):
+        j = i + 1
+        
+        while j < n and name[j] == 'A':
+            j += 1
+        # 오른쪽으로 i까지 갔다가 왼쪽으로 돌아서 j 이후로 가는 케이스들 비교
+        move = min(move, 2 * i + (n-j), i + 2 * (n-j))
+
+    return updown + move
+  
+if __name__ == "__main__":
+    print(solution("JEROEN"))
+    print(solution("JAN"))
